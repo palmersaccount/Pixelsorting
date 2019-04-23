057869dabb54ef1be6c004a092b80cbb8298d5d7
Palmer Brown <wrathofthestorm0@gmail.com>
2019-04-23 09:52:23 -0400

    moved reading things to reading.py

--- pixelsort.py
+++ pixelsort.py
@@ -13,102 +13,11 @@ import interval
 import sorter
 import sorting
 import util
+import reading
 
 
 def clear():
     return os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def read_image_input(url_input):
-    # return order: url, url_given, url_random, random_url
-    try:
-        url = url_input
-        Image.open(requests.get(url, stream=True).raw)
-        return url, True, False, None
-    except IOError:
-        random_url = str(random.randint(0, 5))
-        url_options = {
-            '0': "https://i.imgur.com/1UBT6fK.png",
-            '1': "https://i.imgur.com/5VqeP6Y.jpg?1",
-            '2': "https://i.imgur.com/rfasPI6.jpg?1",
-            '3': "https://i.imgur.com/wFzJ6ua.jpg?1",
-            '4': "https://i.imgur.com/pZRkMzP.jpg?1",
-            '5': "https://i.imgur.com/HwchiaD.jpg?1",
-            '6': "https://i.imgur.com/Z438D1L.jpg"
-        }
-        try:
-            return ((url_options[(url_input if url_input in ['0', '1', '2', '3', '4', '5', '6'] else random_url)] if url_input in ['', ' ', '0', '1', '2', '3', '4', '5', '6'] else url_input),
-                    (False if url_input in [
-                     '', ' ', '0', '1', '2', '3', '4', '5', '6'] else True),
-                    (True if url_input in ['', ' '] else False),
-                    (random_url if url_input in ['', ' '] else None))
-        except KeyError:
-            return url_options[random_url], False, True, random_url
-
-
-def read_interval_function(int_func_input):
-    try:
-        return {
-            "random": interval.random,
-            "threshold": interval.threshold,
-            "edges": interval.edge,
-            "waves": interval.waves,
-            "snap": interval.snap_sort,
-            "file": interval.file_mask,
-            "file-edges": interval.file_edges,
-            "shuffle-total": interval.shuffle_total,
-            "shuffle-axis": interval.shuffled_axis,
-            "none": interval.none,
-        }[int_func_input]
-    except KeyError:
-        return interval.random
-
-
-def read_sorting_function(sort_func_input):
-    try:
-        return {
-            "lightness": sorting.lightness,
-            "hue": sorting.hue,
-            "intensity": sorting.intensity,
-            "minimum": sorting.minimum,
-            "saturation": sorting.saturation,
-        }[sort_func_input]
-    except KeyError:
-        return sorting.lightness
-
-
-def read_preset(preset_input):
-    try:
-        #order-- arg_parse_input, int_func_input, sort_func_input, preset_true, int_rand, sort_rand, shuffled, snapped
-        int_func_input = {
-            '1': 'random',
-            '2': 'threshold',
-            '3': 'edges',
-            '4': 'waves'}
-        sort_func_input = {
-            '1': 'lightness',
-            '2': 'hue',
-            '3': 'intensity',
-            '4': 'minimum',
-            '5': 'saturation'}
-        return {
-            "main": (("-r 50 -c 250 -a 45"), "random", "intensity", True, False, False, False, False),
-            "full random": (("-a "+str(random.randrange(0, 360))+" -c "+str(random.randrange(50, 500, 15))+" -u "+str((random.randrange(50, 100, 5)/100))+" -t "+str((random.randrange(5, 50, 5)/100))+" -r "+str(random.randrange(5, 100, 5))), int_func_input[str(random.randint(1, 4))], sort_func_input[str(random.randint(1, 5))], True, True, True, False, False),
-            "snap-sort": (("-r 50 -c 250 -a 45"), "snap", "intensity", True, False, False, False, True)
-        }[preset_input]
-    except KeyError:
-        print("[WARNING] Invalid preset name, no preset will be applied")
-        return None, None, None, False, None, None, False, False
-
-
-def read_site(site_input):
-    try:
-        return {
-            "put.re": "put.re",
-            "imgur": "imgur"
-        }[site_input]
-    except KeyError:
-        return "put.re"
 
 
 def main():
@@ -122,7 +31,8 @@ def main():
     # url input
     url_input = input(
         "Please input the URL of the image or the default image #:\n(this might take a while depending the image resolution)\n")
-    url, url_given, url_random, random_url = read_image_input(url_input)
+    url, url_given, url_random, random_url = reading.read_image_input(
+        url_input)
     input_img = Image.open(requests.get(url, stream=True).raw)
     width, height = input_img.size
     resolution_msg = "Resolution: "+str(width)+"x"+str(height)
@@ -152,7 +62,7 @@ def main():
                 '3': 'snap-sort'
             }[preset_input]
         # if presets are applied, they take over args
-        arg_parse_input, int_func_input, sort_func_input, preset_true, int_rand, sort_rand, shuffled, snapped = read_preset(
+        arg_parse_input, int_func_input, sort_func_input, preset_true, int_rand, sort_rand, shuffled, snapped = reading.read_preset(
             preset_input)
     else:
         preset_true = False
@@ -232,7 +142,7 @@ def main():
             "1": "put.re",
             "2": "imgur"
         }
-    site_input = read_site(site_input)
+    site_input = reading.read_site(site_input)
     site_msg = ("Image host site: "+site_input)
     clear()
 
@@ -291,8 +201,8 @@ def main():
     __args = p.parse_args(args_in)
 
     output_image_path = "image.png"
-    interval_function = read_interval_function(int_func_input)
-    sorting_function = read_sorting_function(sort_func_input)
+    interval_function = reading.read_interval_function(int_func_input)
+    sorting_function = reading.read_sorting_function(sort_func_input)
     angle = __args.angle
     randomness = __args.randomness
 
